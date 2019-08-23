07c3c9fcc8e933835e50fd665b71db7718b56654
vikas-kundu <47297799+vikas-kundu@users.noreply.github.com>
2019-08-23 13:43:26 +0530

    To install Dependencies

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+clear
+echo "******* AstraTracker Dependencies install in Progress ********"
+echo ""
+echo "=====> Installing BS4 "
+sudo apt install python-bs4 
+echo "=====> Installing Mechanize "
+sudo apt install python-mechanize 
